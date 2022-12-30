e7b4bff51a14743f6e323a47523f8f0d9522a90b
manho30 <manhochan30@gmail.com>
2022-12-31 00:02:38 +0800

    add validation

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ function serveBackendAPI (port=3000){
         const username = req.query.user;
         const password = req.query.p
 
-        if (!username || !password) {
+        if (!username || !password || username.indexOf('==') !== -1 || password.indexOf('==') !== -1) {
             res.status(400).json({
                 'ok': false,
                 'status': 400,
